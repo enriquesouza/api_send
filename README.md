c28910283de946062beeeb394b7fe49134a44d03
enriquesouza <enriquesouza>
2020-03-24 23:34:04 -0300

    Fixes to send with encrypted key

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # api_send
 This API is only to send via NODE.
+
+You must create a .ENV file or just create ENV Vars in your systems.
+
+#If you encrypted your KEY with an key, then Just define it here
+ENCRYPTION_KEY=""
+
+#This is the secret to generate the JWT TOKEN.
+JWT_SECRET=""
+
+Define a API KEY and an API SECRET so you can protect your API from any other APP.
+JWT_API_KEY=""
+JWT_API_SECRET=""
+
+#When you post to your API you must send the following body
+#Body
+{
+	"api_key":"MY_API_KEY",
+	"api_secret":"MY_API_SECRET",
+    "amount": 0.02,
+    "from": "MY_PLAIN_TEXT_ADDRESS",
+    "key": "MY_ENCRYPTED_PRIVATE_KEY",
+    "to": "ADDRESS_WHICH_YOU_WANT_TO_SEND"
+}
+#Return
+{
+    "txid": "ce723e4f6f36aacfb17cc3fc62d8da94a026291f6b3717efb5e9f1acf962de41"
+}
--- controllers/SendController.js
+++ controllers/SendController.js
@@ -4,6 +4,8 @@ import jwt from 'jsonwebtoken'
 const _ = require('lodash');
 const router = express.Router();
 const sendFromSAPI = require('../lib/sapi');
+const decrypt = require('../lib/decrypt');
+
 
 //This method will create a token and send only IF it is a valid TOKEN. This is just to prevent any non Authorized APP to use it.
 //You can create your own way. I just need a quick way to send without saving anything
@@ -14,7 +16,7 @@ router.post('/send', async (req, res) => {
         }
         const app = {
             api_key: process.env.JWT_API_KEY,
-            api_secret: JWT_API_SECRET
+            api_secret: process.env.JWT_API_SECRET
         }
         jwt.sign({
             user: app
@@ -29,19 +31,21 @@ router.post('/send', async (req, res) => {
                         return res.status(403).send(`This method is protected by TOKEN.`);
                     } else {
 
+                        let decryptedPK = decrypt(req.body.key);
+
                         if (_.isUndefined(decryptedPK) || _.isNull(decryptedPK))
                             return res.status(403).send(`Your PK could not be decrypted.`);
 
                         let payLoad = {
                             amount: req.body.amount,
                             from: req.body.from,
-                            key: req.body.key,
+                            key: decryptedPK,
                             to: req.body.to
                         };
                         try {
                             //OK your APP is now authorized to SEND
                             return res.json(await sendFromSAPI(payLoad.to, payLoad.amount, payLoad.key));
-                        }catch (err) {
+                        } catch (err) {
                             console.error(err);
                             return res.status(400).send(`Error while trying to SEND -> ${err.message}`);
                         }
@@ -51,7 +55,22 @@ router.post('/send', async (req, res) => {
         });
     } catch (err) {
         console.error(err);
-        return res.status(400).send(`Error while getting the TOKEN`);
+        return res.status(400).send(`${err.message}`);
     }
 });
+
+
+//This method will create a token and send only IF it is a valid TOKEN. This is just to prevent any non Authorized APP to use it.
+//You can create your own way. I just need a quick way to send without saving anything
+/*router.post('/decrypt', async (req, res) => {
+    try {
+        if (_.isUndefined(req.body)) {
+            return res.status(403).send(`You must have an API KEY and an API Secret in order to get the TOKEN`);
+        }
+        return res.json(decrypt(req.body.key));
+    } catch (err) {
+        console.error(err);
+        return res.status(400).send(`Error while getting the TOKEN`);
+    }
+});*/
 module.exports = router;
