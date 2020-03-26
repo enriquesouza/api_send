bac2852f5c96e7122c24932a423a60d6c4348e94
enriquesouza <enriquesouza>
2020-03-26 16:39:52 -0300

    change to port 5000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app.js');
-const port = process.env.port || 80;
+const port = process.env.port || 5000;
 app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`);
 });
